f259394ca97236ce7d3e6ec27fca599260a6753e
dyh_a <xinzon32177@163.com>
2024-02-29 13:15:37 +0800

    chore(eslint): 修复 eslint 插件冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    //    'plugin:@typescript-eslint/recommended',
     'react-app',
     'plugin:jsx-a11y/recommended',
     'prettier',
